277d3e5689b2b36edc42af422747044a7534fcff
Gaby Nieva <gaby.nieva@outlook.com>
2025-03-10 00:02:14 -0600

    Header cuadrado

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:custom_painter/src/pages/headers_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,11 +11,9 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+      debugShowCheckedModeBanner: false,
+      title: 'Diseños con Custom Painter',
+      home: HeadersPage(),
     );
   }
 }
